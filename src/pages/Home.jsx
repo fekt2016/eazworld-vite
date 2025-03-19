c5372e32a5ad54a057aa88f84211fe0d3b2407c1
yussif <fekt2016@icloud.com>
2025-03-19 12:04:51 +0000

    changing the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,15 @@
-import Header from '../features/home/Header'
-import Service from '../ui/Service'
-import StyledSection from '../features/home/Section'
-import RateCard from '../features/home/RateCard'
+// import Header from '../features/home/Header'
+// import Service from '../ui/Service'
+// import StyledSection from '../features/home/Section'
+// import RateCard from '../features/home/RateCard'
 
-import Footer from '../features/home/Footer'
-import NewSteps from '../features/home/NewSteps'
-import Safe from '../features/home/Safe'
-import Whatsapp from '../ui/Whatsapp'
-import Alert from '../ui/Alert'
+// import Footer from '../features/home/Footer'
+// import NewSteps from '../features/home/NewSteps'
+// import Safe from '../features/home/Safe'
+// import Whatsapp from '../ui/Whatsapp'
+// import Alert from '../ui/Alert'
 function Home() {
-  return (
-    <>
-      <Alert />
-      <Header />
-      <Service />
-      <Safe />
-      <RateCard />
-      <StyledSection type="step">
-        <NewSteps />
-      </StyledSection>
-      <Footer />
-      <Whatsapp />
-    </>
-  )
+  return <h1> Eaz shop is now under maintaince</h1>;
 }
 
-export default Home
+export default Home;
