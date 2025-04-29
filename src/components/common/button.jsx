2e2afd4694edf12b426c0756f107422fd6741685
Ykay-cloud <yusifyushau4@gmail.com>
2025-04-29 13:38:07 +0000

    Update button.jsx

--- src/components/common/button.jsx
+++ src/components/common/button.jsx
@@ -20,31 +20,10 @@ white-space: none;
 `;
 
 
-const SearchEngine = styled.button`
-color: #474747;
-background-color: #fff;
-border-radius: 10px;
-font-size: 14px;
-border: 1px solid #716D6D;
-/* border: none; */
-/* box-shadow: none; */
-height: 45px;
-width: 650px;
-margin-left: 15px;
-/* text-align: left; */
-display: flex;
-justify-content: space-between;
 
-
-
-
-
-
-`;
 
 export {Button};
 
-export {SearchEngine};
 
 
 
